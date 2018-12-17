735be198266e0e912fd1bee08b4a7c25c8ca0c5f
Ronyonka <rononyonka@gmail.com>
2018-12-17 07:19:45 +0300

    added styling

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 // Business logic
-function rollDice(){
-  var total = 0;
+function rollDice() {
+  var summation = 0;
   var dice = Math.floor(Math.random() * 6) + 1;
 
-  if (dice != 1){
-    total += dice
-    alert(total);
-  }else{
+  if (dice != 1) {
+    summation += dice
+    $("#currentScore").text(summation);
+  } else {
     alert('you rolled a one')
   }
 }
+
+// end of my code
